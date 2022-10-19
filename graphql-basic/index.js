08de71472defb920effbd0a2373c5c3764bc3e64
salvadorjmz24 <hector.salvador.lopez.jimenez@gmail.com>
2022-10-19 14:19:27 -0500

    clase del dia 18 se agrego data.json y al index se le añidio delete, update :fire:

--- graphql-basic/index.js
+++ graphql-basic/index.js
@@ -3,12 +3,26 @@ const app = express();
 
 const { buildSchema } = require( 'graphql' );
 const { graphqlHTTP } = require( 'express-graphql' );
-
+const {courses} = require('./data.json')
 
 const schema = buildSchema( `
     type Query {
         getWelcome(name: String): String
-        getResponse : Boolean
+        getCourses : [Course]
+        findCoursesById(id: Int): Course
+    }
+
+    type Mutation {
+        addCourse(id: Int, title: String!, level: String , date: String) : [Course]
+        updateCourse(id: Int, title: String!, level: String , date: String) : Course
+        deleteCourse(id: Int): [Course]
+    }
+
+    type Course {
+        id: Int,
+        title: String,
+        level: String,
+        date: String
     }
 
 `);
@@ -16,17 +30,51 @@ const schema = buildSchema( `
 const getWelcome = (args) => {
     return "hello word " +  args.name;
 };
-
-const getResponse = () => {
-    return true;
+const getCourses = () => {
+    return courses;
+}
+const addCourse = ( { id, title, level, date } ) => {
+    const newCourse = { id: id, title: title, level: level, date: date };
+    courses.push(newCourse)
+    return courses; 
 }
 
+const updateCourse = ( { id, title, level, date}) => {
+    //Find the course object to update
+    courses.map( course => {
 
+        if ( course.id == id ) {
+            course.title = title ? title : course.title; //if else
+            course.level = level ? level : course.level;
+            course.date = date ? date : new Date();
+        }
+        return course;
+    });
+}
 
+const findCoursesById = ( { id }) => {
+    return courses.find(course => course.id == id)
+}
+
+//Delete
+ const deleteCourse = ( { id } ) => {
+    let courseId = id;
+    var getIndex = courses.findIndex( line => line.id === courseId );
+    console.log(getIndex)
+    //remove the object
+    courses.splice(getIndex, 1)
+    //return courses []
+    return courses
+ }
 const root = {
     //properties(Shema) = functions
     getWelcome: getWelcome,
-    getResponse : getResponse
+    getCourses: getCourses,
+    addCourse: addCourse,
+    updateCourse: updateCourse,
+    findCoursesById: findCoursesById,
+    deleteCourse: deleteCourse
+
 }
 
 app.use( '/graphql', graphqlHTTP ( {
